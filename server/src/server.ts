4a8441f0f6020193251fab716ec69324c5d8b3f5
JUN HYEOK KIM <kjh45388117@gmail.com>
2023-11-12 15:27:54 +0900

    remove nginx

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,8 +3,7 @@ import express, { Request, Response } from "express";
 import prisma from "./lib/prisma";
 import cors from "cors";
 
-import { createTunnel } from "tunnel-ssh";
-
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import {
   S3Client,
   PutObjectCommand,
@@ -12,7 +11,6 @@ import {
   DeleteObjectsCommand,
   GetObjectCommand,
 } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 import getProductDetail from "./routes/getProductDetail";
 import getArtwork from "./routes/getArtwork";
