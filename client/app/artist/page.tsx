d4c614f04143540e44b8481ed99033d504a5b285
JUN HYEOK KIM <kjh45388117@gmail.com>
2023-11-30 18:14:34 +0900

    error

--- client/app/artist/page.tsx
+++ client/app/artist/page.tsx
@@ -1,12 +1,12 @@
-import CollaboArtist from "@/src/components/Artist/CollaboArtist";
-import React from "react";
+// import CollaboArtist from "@/src/components/Artist/CollaboArtist";
+// import React from "react";
 
-const page = () => {
-  return (
-    <div>
-      <CollaboArtist />
-    </div>
-  );
-};
+// const page = () => {
+//   return (
+//     <div>
+//       <CollaboArtist />
+//     </div>
+//   );
+// };
 
-export default page;
+// export default page;
