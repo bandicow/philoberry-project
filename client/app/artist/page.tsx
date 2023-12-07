31538698559e9233d3368f942b9d3fa3b2dba642
JUN HYEOK KIM <kjh45388117@gmail.com>
2023-12-07 16:29:04 +0900

    generateMeta test

--- client/app/artist/page.tsx
+++ client/app/artist/page.tsx
@@ -9,39 +9,56 @@ type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-// export async function generateMetadata(
-//   { params, searchParams }: Props,
-//   parent: ResolvingMetadata
-// ): Promise<Metadata> {
-//   const { name, major, artist_image, profile, website_url } =
-//     await getCollaboArtist();
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  let name, major, artist_image, profile, website_url;
 
-//   const previousImages = (await parent).openGraph?.images || [];
+  try {
+    const result = await getCollaboArtist();
 
-//   return {
-//     title: `${name} | Philoberry`,
-//     description: profile,
-//     keywords: `${name}, ${major}, ${profile}, ${website_url} , 무명 , 작가 ,무명 작가, philoberry, 필로베리, 휠로베리`,
+    name = result.name;
+    major = result.major;
+    artist_image = result.artist_image;
+    profile = result.profile;
+    website_url = result.website_url;
+  } catch (error) {
+    console.error("Failed to get collabo artist:", error);
+    // If an error occurs, use default values
+    name = "Default";
+    major = "Default";
+    artist_image = "https://philoberry.com/images/35mm_logo.png";
+    profile = "Default profile";
+    website_url = "https://www.philoberry.com";
+  }
 
-//     openGraph: {
-//       type: "website",
-//       url: "https://www.philoberry.com",
-//       title: `${name} | Philoberry`,
-//       description: profile ? profile : "작가의 프로필이 없습니다.",
-//       images: [
-//         {
-//           url: artist_image
-//             ? artist_image
-//             : "https://philoberry.com/images/35mm_logo.png",
-//           width: 500,
-//           height: 500,
-//           alt: "작가사진",
-//         },
-//         ...previousImages,
-//       ],
-//     },
-//   };
-// }
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: `${name} | Philoberry`,
+    description: profile,
+    keywords: `${name}, ${major}, ${profile}, ${website_url} , 무명 , 작가 ,무명 작가, philoberry, 필로베리, 휠로베리`,
+
+    openGraph: {
+      type: "website",
+      url: "https://www.philoberry.com",
+      title: `${name} | Philoberry`,
+      description: profile ? profile : "작가의 프로필이 없습니다.",
+      images: [
+        {
+          url: artist_image
+            ? artist_image
+            : "https://philoberry.com/images/35mm_logo.png",
+          width: 500,
+          height: 500,
+          alt: "작가사진",
+        },
+        ...previousImages,
+      ],
+    },
+  };
+}
 
 const page = () => {
   return (
